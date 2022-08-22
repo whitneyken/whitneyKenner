e8e8f1298d64d4a9dde3b3a5125d54f88efe97c9
whitney <kennerwhitney@gmail.com>
2022-08-22 15:54:44 -0600

    committing helloworld to repo

---
+++ cs6010/day1/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Whitney Kenner on 8/22/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "Happy Monday!\n";
+    std::cout << "learning C++\n";
+    int num1 = 2, num2 = 8, sum;
+    sum = num1 + num2;
+    std::cout << sum;
+    std::cout << "\n";
+    
+    return 0;
+}
