d6ea5c9b324cc9d3b3a3ed0d949ab7d38aa5bcbb
whitney <kennerwhitney@gmail.com>
2022-08-24 12:57:51 -0600

    committing if practice lab to git

--- cs6010/Day3/IfStatementPractice/IfStatementPractice/IfStatementPractice/main.cpp
+++ cs6010/Day3/IfStatementPractice/IfStatementPractice/IfStatementPractice/main.cpp
@@ -27,17 +27,49 @@ int main(int argc, const char * argv[]) {
     if (userAge > 80){
         std::cout <<"\n You are part of the greatest generation! \n";
     }
-    else if (userAge > 60 && userAge < 80){
+    else if (userAge > 60 && userAge <= 80){
         std::cout <<"\n I'm sorry, you are a baby boomer! \n";
     }
-    else if (userAge > 40 && userAge < 60){
+    else if (userAge > 40 && userAge <= 60){
         std::cout <<"\n You are part of generation X! \n";
     }
-    else if (userAge > 20 && userAge < 40){
+    else if (userAge > 20 && userAge <= 40){
         std::cout <<"\n You are a millenial! \n";
     }
     else{
         std::cout <<"\n You are an iKid! \n";
     }
+    //Part 2 of lab
+    //now recieve user input on what day it is
+    char isTodayWeekday;
+    char isTodayHoliday;
+    char doesUserHaveChildren;
+    
+    do {
+        std::cout << "\n Is it a weekday? (Y/N): ";
+        std::cin >> isTodayWeekday;
+    } while (isTodayWeekday != 'Y' && isTodayWeekday != 'N' && isTodayWeekday != 'y' && isTodayWeekday != 'n');
+    do {
+        std::cout << "\n Is it a holiday? (Y/N): ";
+        std::cin >> isTodayHoliday;
+    } while (isTodayHoliday != 'Y' && isTodayHoliday != 'N' && isTodayHoliday != 'y' && isTodayHoliday != 'n');
+    do {
+        std::cout <<("\n Do you have young children? (Y/N): ");
+        std::cin >> doesUserHaveChildren;
+    } while (doesUserHaveChildren != 'Y' && doesUserHaveChildren != 'N' && doesUserHaveChildren != 'y' && doesUserHaveChildren != 'n');
+    /*Now we are going to make a bunch of if else statements to figure oout if this person gets to sleep in*/
+    
+    bool yesChildren = (doesUserHaveChildren == 'Y' || doesUserHaveChildren == 'y');
+    bool isWeekday = (isTodayWeekday == 'Y' || isTodayWeekday == 'y');
+    bool isNotHoliday = (isTodayHoliday == 'N' || isTodayHoliday == 'n');
+    
+    if (yesChildren) {
+        std::cout <<("\n You do not get to sleep in today. \n");
+    }else if (isWeekday && isNotHoliday){
+        std::cout <<("\n You do not get to sleep in today. \n");
+    }else{
+        std::cout <<("\n Congratulations! You get to sleep in! \n");
+    }
+             
     return 0;
 }
