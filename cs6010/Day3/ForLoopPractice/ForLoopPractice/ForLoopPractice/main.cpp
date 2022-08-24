9f5b505b66d35a18864325eb670f723917d21800
whitney <kennerwhitney@gmail.com>
2022-08-24 15:14:17 -0600

    removed thing making all my code a GD comment

--- cs6010/Day3/ForLoopPractice/ForLoopPractice/ForLoopPractice/main.cpp
+++ cs6010/Day3/ForLoopPractice/ForLoopPractice/ForLoopPractice/main.cpp
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     // starting by printing the numbers 1-10 using a for loop
 
-    /* for (int forCounter = 1; forCounter <= 10; forCounter++) {
+    for (int forCounter = 1; forCounter <= 10; forCounter++) {
         std::cout << forCounter << "\n";
     }
     //next we print the numbers but in a while loop
@@ -38,7 +38,7 @@ int main(int argc, const char * argv[]) {
     
      /* Now print all the odd numbers between 1 and 20. come up with 2 solutions, that uses a loop and an if statement, and 1 that doesn't require an if statement. which is better? */
     
-        /*for (int oddNumberCounter = 1; oddNumberCounter <= 20; oddNumberCounter += 2) {
+        for (int oddNumberCounter = 1; oddNumberCounter <= 20; oddNumberCounter += 2) {
             std::cout << oddNumberCounter << "\n";
         }
     int whileOddNumberCounter = 1;
@@ -51,7 +51,7 @@ int main(int argc, const char * argv[]) {
     //The for loop is MUCH more simple in my opinion, the layered loop/if statement is dumb
     /*Ask a user to enter positive numbers to add up. Keep reading and adding numbers until the user enters a number that is less than 0, then print the sum. For example, if the user entered 1 2 3 -1, you should print 6. This one is a little tricky!*/
     
-   /*int newNumberEntered = 0;
+   int newNumberEntered = 0;
     int newNumberPlusOldNumbers = 0;
     while (newNumberEntered >= 0) {
         std::cout <<"\n Enter a number: ";
@@ -60,7 +60,7 @@ int main(int argc, const char * argv[]) {
             newNumberPlusOldNumbers = newNumberPlusOldNumbers + newNumberEntered;
         }
     }
-    std::cout<<"\n The total addition of your numbers is: " << newNumberPlusOldNumbers << std::endl;*/
+    std::cout<<"\n The total addition of your numbers is: " << newNumberPlusOldNumbers << std::endl;
     
     //making a multiplication table
     for (int y = 1; y <= 5; y++) {
