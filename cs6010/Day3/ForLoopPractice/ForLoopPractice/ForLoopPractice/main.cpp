a1427963f2437326d3cf558b57005a89039b91ef
whitney <kennerwhitney@gmail.com>
2022-08-24 18:19:34 -0600

    fixed some small things

--- cs6010/Day3/ForLoopPractice/ForLoopPractice/ForLoopPractice/main.cpp
+++ cs6010/Day3/ForLoopPractice/ForLoopPractice/ForLoopPractice/main.cpp
@@ -31,16 +31,15 @@ int main(int argc, const char * argv[]) {
         std::cin >> secondEnteredNumber;
     } while (firstEnteredNumber > secondEnteredNumber);
     while (firstEnteredNumber <= secondEnteredNumber) {
-        std::cout <<firstEnteredNumber << std::endl;
+        std::cout << firstEnteredNumber << std::endl;
         firstEnteredNumber++;
-    
     }
     
      /* Now print all the odd numbers between 1 and 20. come up with 2 solutions, that uses a loop and an if statement, and 1 that doesn't require an if statement. which is better? */
     
-       for (int oddNumberCounter = 1; oddNumberCounter <= 20; oddNumberCounter += 2) {
-            std::cout << oddNumberCounter << "\n";
-        }
+   for (int oddNumberCounter = 1; oddNumberCounter <= 20; oddNumberCounter += 2) {
+        std::cout << oddNumberCounter << "\n";
+    }
     int whileOddNumberCounter = 1;
     while (whileOddNumberCounter <= 20) {
         if (whileOddNumberCounter % 2 != 0) {
@@ -49,15 +48,16 @@ int main(int argc, const char * argv[]) {
         whileOddNumberCounter++;
     }
     //The for loop is MUCH more simple in my opinion, the layered loop/if statement is dumb
+    
     /*Ask a user to enter positive numbers to add up. Keep reading and adding numbers until the user enters a number that is less than 0, then print the sum. For example, if the user entered 1 2 3 -1, you should print 6. This one is a little tricky!*/
     
     int newNumberEntered = 0;
     int newNumberPlusOldNumbers = 0;
     while (newNumberEntered >= 0) {
-        std::cout <<"\n Enter a number: ";
-        std::cin>> newNumberEntered;
+        std::cout << "\n Enter a number: ";
+        std::cin >> newNumberEntered;
         if (newNumberEntered >= 0) {
-            newNumberPlusOldNumbers = newNumberPlusOldNumbers + newNumberEntered;
+            newNumberPlusOldNumbers += newNumberEntered;
         }
     }
     std::cout<<"\n The total addition of your numbers is: " << newNumberPlusOldNumbers << std::endl;
@@ -66,7 +66,6 @@ int main(int argc, const char * argv[]) {
     for (int y = 1; y <= 5; y++) {
         std::cout<< y << "x*:  ";
         for (int x = 1; x <= 5; x++){
-            //std::cout << x * y << " ";
             if ((x * y) < 10) {
                 std::cout<<" " << x * y << " ";
             }
