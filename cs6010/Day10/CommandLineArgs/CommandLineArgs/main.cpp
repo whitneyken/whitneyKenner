afe00bf2107008a17fce0a88c8d3552643f1e9de
whitney <kennerwhitney@gmail.com>
2022-09-02 12:14:05 -0600

    Command line args

---
+++ cs6010/Day10/CommandLineArgs/CommandLineArgs/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  CommandLineArgs
+//
+//  Created by Whitney Kenner on 9/2/22.
+//
+
+#include <iostream>
+
+int main(int argc,  char ** argv) {
+    //printing out the command line arguments!!!!!!
+    //TO THE COMMAND LINE
+    
+    /* the first element of the argv array is "./main" since that is the first thing we enter. so we could can have it start with 1 to avoid printing that out again*/
+    
+    for (int i = 1; i < argc; i++) {
+        std::cout << argv[i] << " ";
+    }
+    return 0;
+}
