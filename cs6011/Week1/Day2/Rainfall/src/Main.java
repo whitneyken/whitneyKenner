e3ce1f6c604c8edc52a90f955c1494ea8ac1b7fc
whitney <kennerwhitney@gmail.com>
2022-09-28 14:46:27 -0600

    idk, not working

---
+++ cs6011/Week1/Day2/Rainfall/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public Main() throws FileNotFoundException {
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Hello world!");
+
+    String fileName = new String("src/rainfall_data.txt");
+    RainData myData;
+        myData = new RainData(fileName);
+        double fourth = myData.rainFall.get(3);
+        System.out.println(fourth);
+}
+}
--- cs6011/Week1/Day2/Rainfall/src/RainData.java
+++ cs6011/Week1/Day2/Rainfall/src/RainData.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class RainData {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.*;
+import java.util.regex.MatchResult;
+
+public class RainData {
+    String cityName;
+    ArrayList<String> month = new ArrayList<String>();
+    ArrayList<Integer> year = new ArrayList<Integer>();
+    ArrayList<Double> rainFall = new ArrayList<Double>();
+
+
+    public RainData(String filename) throws FileNotFoundException {
+        //File myObj = new File("src/rainfall_data.txt"); //tried both this one and the line below and neither work
+        FileInputStream fin = new FileInputStream("rainfall_data.txt");
+        Scanner myReader = new Scanner(fin);
+        cityName = myReader.nextLine();
+        String oneMonth = new String();
+        int oneYear;
+        double oneRainFall;
+        for (int i = 0; i < 239; i++){
+            oneMonth = myReader.nextLine();
+            month.add(oneMonth);
+            oneYear = myReader.nextInt();
+            year.add(oneYear);
+            oneRainFall = myReader.nextDouble();
+            rainFall.add(oneRainFall);
+        }
+    }
 }
