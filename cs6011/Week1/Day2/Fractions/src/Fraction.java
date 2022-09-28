0147132a96c41603caac4cff6586464e65d56c4a
whitney <kennerwhitney@gmail.com>
2022-09-28 13:52:36 -0600

    pretty fraction updates

--- cs6011/Week1/Day2/Fractions/src/Fraction.java
+++ cs6011/Week1/Day2/Fractions/src/Fraction.java
@@ -11,8 +11,8 @@ public class Fraction {
     //This constructor will set the value of the numerator and denominator to input values
     public Fraction(long n, long d) {
         if (d < 0) {
-            n *= -1;
-            d *= -1;
+            numerator *= -1;
+            denominator *= -1;
         }
         numerator = n;
         denominator = d;
@@ -23,77 +23,51 @@ public class Fraction {
     //This is the beginning of my methods
     //This method makes a sum of the 2 fractions and returns a fraction
     public Fraction Plus(Fraction rhs) {
-        Fraction tempFrac = new Fraction();
-        if (denominator == rhs.denominator) {
-            tempFrac.denominator = rhs.denominator;
-            tempFrac.numerator = (numerator + rhs.numerator);
-            tempFrac.reduce();
-            return tempFrac;
-        } else {
-            tempFrac.denominator = (denominator * rhs.denominator);
-            tempFrac.numerator = ((numerator * rhs.denominator) + (rhs.numerator * denominator));
-            tempFrac.reduce();
-            return tempFrac;
-        }
+        long tempN = ((numerator * rhs.denominator) + (rhs.numerator * denominator));
+        long tempD = (denominator * rhs.denominator);
+        Fraction tempFrac = new Fraction(tempN, tempD);
+        return tempFrac;
 
     }
 
     //Returns a new fraction that is the result of the right hand side (rhs) fraction subtracted from this fraction.
     public Fraction Minus(Fraction rhs) {
-        Fraction tempFrac = new Fraction();
-        if (denominator == rhs.denominator) {
-            tempFrac.denominator = rhs.denominator;
-            tempFrac.numerator = numerator - rhs.numerator;
-            tempFrac.reduce();
-            return tempFrac;
-        } else {
-            tempFrac.denominator = (denominator * rhs.denominator);
-            tempFrac.numerator = ((numerator * rhs.denominator) - (rhs.numerator * denominator));
-        }
-        tempFrac.reduce();
+        long tempN = ((numerator * rhs.denominator) - (rhs.numerator * denominator));
+        long tempD = (denominator * rhs.denominator);
+        Fraction tempFrac = new Fraction(tempN, tempD);
         return tempFrac;
-
     }
 
     //Returns a new fraction that is the result of this fraction multiplied by the right hand side (rhs) fraction.
     public Fraction Times(Fraction rhs) {
-        Fraction tempFrac = new Fraction();
-        tempFrac.numerator = (numerator * rhs.numerator);
-        tempFrac.denominator = (denominator * rhs.denominator);
-        tempFrac.reduce();
+        long tempN = (numerator * rhs.numerator);
+        long tempD = (denominator * rhs.denominator);
+        Fraction tempFrac = new Fraction(tempN, tempD);
         return tempFrac;
     }
 
     //Returns a new fraction that is the result of this fraction divided by the right hand side (rhs) fraction.
     public Fraction DividedBy(Fraction rhs) {
         Fraction reciprocal = new Fraction();
-        Fraction tempFrac = new Fraction();
         reciprocal = rhs.reciprocal();
-        tempFrac.numerator = (numerator * reciprocal.numerator);
-        tempFrac.denominator = (denominator * reciprocal.denominator);
-        tempFrac.reduce();
+        long tempN = (numerator * reciprocal.numerator);
+        long tempD = (denominator * reciprocal.denominator);
+        Fraction tempFrac = new Fraction(tempN, tempD);
         return tempFrac;
     }
 
 
     //Returns a new fraction that is the reciprocal of this fraction.
     public Fraction reciprocal() {
-        Fraction tempFrac = new Fraction();
-        long tempD = denominator;
-        tempFrac.denominator = numerator;
-        tempFrac.numerator = tempD;
+        Fraction tempFrac = new Fraction(denominator, numerator);
         return tempFrac;
-
     }
 
     //Returns a string representing this fraction
     //Cannot use the method "toString", because it cannot accept longs, so using valueOf (accepting longs specifically)
     //to convert longs to strings and concatenate with the "/" and the denominator
     public String toString() {
-        String stringFraction;
-        stringFraction = String.valueOf(numerator);
-        stringFraction += "/";
-        stringFraction += String.valueOf(denominator);
+        String stringFraction = (numerator + "/" + denominator);
         return stringFraction;
 
     }
@@ -101,8 +75,8 @@ public class Fraction {
     //Returns a (double precision) floating point number that is the approximate
     // value of this fraction, printed as a real number.
     public double toDouble() {
-        double fracResult = 0.0000000;
-        fracResult = ((numerator * 1.000) / (denominator * 1.000));
+        double fracResult;
+        fracResult = ((numerator * 1.000) / (denominator));
         return fracResult;
     }
 
